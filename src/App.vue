86bab478c702db5932c819ee60ccaad08ae04f44
John Learned <Nightfall449@gmail.com>
2022-12-02 11:05:04 -0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <button v-on:click="toggle='character-generator'">View All Characters </button>
+        <button v-on:click="toggle='character-creator'">Create A Character </button>
+        <CharacterGenerator v-show="toggle==='character-generator'"/> 
+        <CharacterCreator v-show="toggle==='character-creator'" /> <!-- Tells Vue to toggle between the v-on: clicks-->
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import CharacterGenerator from './components/CharacterGenerator.vue'
+    import CharacterCreator from "./components/CharacterCreator.vue"
+    export default {
+        name: 'App',
+        components: {
+            CharacterGenerator,
+            CharacterCreator
+        },
+        data: function(){
+            return { // returns an object
+                toggle: "character-generator" //Stores data locally on the application.
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        background-color: dodgerblue;
+        color: black;
+    }
 </style>
---
+++ src/components/CharacterCreator.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="character-creator">
+        <h1>Character Creator</h1>
+        <label for="character-name">Character Name: </label>
+        <input type="text" id="character-name" v-model="name" placeholder="Enter a character name" /><br /><br /> <!-- breaks added-->
+        <label for="character-class-list">Character Class</label>
+
+        <select id="character-class-list" v-model="occupation">
+            <option value="Warrior">Warrior</option>
+            <option value="Wizard">Wizard</option>
+            <option value="Rogue">Rogue</option>
+            <option value="Adventurer">Adventurer</option>
+            <option value="Mage">Mage</option>
+
+        </select>
+        <label for="charater-weapon-choice">Weapon Choice</label>
+          <select id="character-weapon-choice" v-model="weapons">
+              <option value="Long Sword">Long Sword</option>
+              
+          </select>
+        <p> {{ name }} </p>
+        <p> {{ occupation }} </p>
+        <p> {{ weapons }} </p>
+    </div> <!-- v-model links data to the name-->
+</template>
+
+<script>
+    export default {
+        name: 'CharacterCreator',
+        data: function () { // Added the data function which returns an object.
+            return {
+                name: "", //input is pointing to this variable in the data object.
+                occupation: "",
+                weapons: ""
+      
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
